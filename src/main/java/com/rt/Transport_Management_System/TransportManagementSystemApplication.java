40cb9aae2673a2b2beff020e36393b025049bdfa
Vaibhav Irkar <vaibhavirkar8767@gmail.com>
2025-09-28 23:02:32 -0700

    first commit

---
+++ src/main/java/com/rt/Transport_Management_System/TransportManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rt.Transport_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportManagementSystemApplication.class, args);
+	}
+
+}
