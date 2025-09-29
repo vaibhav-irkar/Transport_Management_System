8914af00a44281b31d51ca4cfa4f2b8180706ac2
tushark2004 <tusharkeskar55@gmail.com>
2025-09-29 11:33:20 +0530

    Massage Hii

--- src/main/java/com/rt/Transport_Management_System/TransportManagementSystemApplication.java
+++ src/main/java/com/rt/Transport_Management_System/TransportManagementSystemApplication.java
@@ -8,6 +8,7 @@ public class TransportManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransportManagementSystemApplication.class, args);
+		System.out.println("hii...");
 	}
 
 }
